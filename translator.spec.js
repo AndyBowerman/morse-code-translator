56a9e93a58e7a0ea807abb23fb3101f3a0f4cb59
Andy Bowerman <andy_bowerman06@hotmail.com>
2022-09-02 10:01:54 +0100

    first commit - tests on lowercase alphabet

---
+++ translator.spec.js
@@ -0,0 +1,127 @@
+import { translateToMorse } from "./translator";
+
+/*
+this the function should do...
+convert letters
+convert capitals oe lowercase letters the same
+convert numbers
+convert punctuation
+convert spaces to /
+return ! if symbol is unrecognised
+return a string
+
+*/
+
+
+
+describe("Testing translateToMorse", () => {
+    it("should turn 'a' into morse code", () => {
+        let result = translateToMorse('a')
+        expect(number).toBe('.-')
+    })
+    it("should turn 'b' into morse code", () => {
+        let result = translateToMorse('b')
+        expect(number).toBe('-...')
+    })
+    it("should turn 'c' into morse code", () => {
+        let result = translateToMorse('c')
+        expect(number).toBe('-.-.')
+    })
+    it("should turn 'd' into morse code", () => {
+        let result = translateToMorse('d')
+        expect(number).toBe('-..')
+    })
+    it("should turn 'e' into morse code", () => {
+        let result = translateToMorse('e')
+        expect(number).toBe('.')
+    })
+    it("should turn 'f' into morse code", () => {
+        let result = translateToMorse('f')
+        expect(number).toBe('..-.')
+    })
+    it("should turn 'g' into morse code", () => {
+        let result = translateToMorse('g')
+        expect(number).toBe('--.')
+    })
+    it("should turn 'h' into morse code", () => {
+        let result = translateToMorse('h')
+        expect(number).toBe('....')
+    })
+    it("should turn 'i' into morse code", () => {
+        let result = translateToMorse('i')
+        expect(number).toBe('..')
+    })
+    it("should turn 'j' into morse code", () => {
+        let result = translateToMorse('j')
+        expect(number).toBe('.---')
+    })
+    it("should turn 'k' into morse code", () => {
+        let result = translateToMorse('k')
+        expect(number).toBe('-.-')
+    })
+    it("should turn 'l' into morse code", () => {
+        let result = translateToMorse('l')
+        expect(number).toBe('.-..')
+    })
+    it("should turn 'm' into morse code", () => {
+        let result = translateToMorse('m')
+        expect(number).toBe('--')
+    })
+    it("should turn 'n' into morse code", () => {
+        let result = translateToMorse('n')
+        expect(number).toBe('-.')
+    })
+    it("should turn 'o' into morse code", () => {
+        let result = translateToMorse('o')
+        expect(number).toBe('---')
+    })
+    it("should turn 'p' into morse code", () => {
+        let result = translateToMorse('p')
+        expect(number).toBe('.--.')
+    })
+    it("should turn 'q' into morse code", () => {
+        let result = translateToMorse('q')
+        expect(number).toBe('--.-')
+    })
+    it("should turn 'r' into morse code", () => {
+        let result = translateToMorse('r')
+        expect(number).toBe('.-.')
+    })
+    it("should turn 's' into morse code", () => {
+        let result = translateToMorse('s')
+        expect(number).toBe('...')
+    })
+    it("should turn 't' into morse code", () => {
+        let result = translateToMorse('t')
+        expect(number).toBe('-')
+    })
+    it("should turn 'u' into morse code", () => {
+        let result = translateToMorse('u')
+        expect(number).toBe('..-')
+    })
+    it("should turn 'v' into morse code", () => {
+        let result = translateToMorse('v')
+        expect(number).toBe('...-')
+    })
+    it("should turn 'w' into morse code", () => {
+        let result = translateToMorse('w')
+        expect(number).toBe('.--')
+    })
+    it("should turn 'x' into morse code", () => {
+        let result = translateToMorse('x')
+        expect(number).toBe('-..-')
+    })
+    it("should turn 'y' into morse code", () => {
+        let result = translateToMorse('y')
+        expect(number).toBe('-.--')
+    })
+    it("should turn 'z' into morse code", () => {
+        let result = translateToMorse('z')
+        expect(number).toBe('--..')
+    })
+
+
+
+
+
+})
