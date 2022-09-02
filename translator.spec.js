724d4e2cf492b0e34d77da507bec635970563b65
Andy Bowerman <andy_bowerman06@hotmail.com>
2022-09-02 14:32:09 +0100

    tests written for converting morse code back to english

--- translator.spec.js
+++ translator.spec.js
@@ -46,13 +46,49 @@ describe("Testing translateToMorse", () => {
         let result = translateToMorse('£4.99')
         expect(result).toBe("Warning, unknown symbol: ! ....- .-.-.- ----. ----.")
     })
+
+    it("can convert numerous full sentences", () => {
+        let result = translateToMorse('The tree was tall. The cat was loud.')
+        expect(result).toBe('- .... . / - .-. . . / .-- .- ... / - .- .-.. .-.. .-.-.- / - .... . / -.-. .- - / .-- .- ... / .-.. --- ..- -.. .-.-.-')
+    })
 })
 
 
 
-
-
-
 describe("Testing morseToEnglish", () => {
+    it("capitalises the first letter in each sentence", () => {
+        let result = morseToEnglish(".... . .-.. .-.. ---")
+        expect(result).toBe('Hello')
+    })
+
+    it("correctly recognises spaces between each letter and / between each word to return a sentence", () => {
+        let result = morseToEnglish("--. --- --- -.. / -- --- .-. -. .. -. --. -.-.--")
+        expect(result).toBe('Good morning!')
+    })
+
+    it("convert morse code to numbers", () => {
+        let result = morseToEnglish(".---- / ..--- / ...-- / ....- / ..... / -.... / --... / ---.. / ----. / -----")
+        expect(result).toBe("1 2 3 4 5 6 7 8 9 0")
+    })
+
+    it("convert morse code to letters", () => {
+        let result = morseToEnglish('.- / -... / -.-. / -.. / . / ..-. / --. / .... / .. / .--- / -.- / .-.. / -- / -. / --- / .--. / --.- / .-. / ... / - / ..- / ...- / .-- / -..- / -.-- / --..')
+        expect(result).toBe('A b c d e f g h i j k l m n o p q r s t u v w x y z')
+    })
+
+    it("convert morse code into punctuation", () => {
+        let result = morseToEnglish(".-.-.- / --..-- / ..--.. / .---. / -.-.-- / -..-. / -.--. / -.--.- / .-... / ---... / -.-.-. / -...- / .-.-. / -....- / ..--.- / .-..-. / .--.-.")
+        expect(result).toBe(". , ? ' ! / ( ) & : ; = + - _ \" @")
+    })
+
+    it("doesn't return anything for unknown symbols", () => {
+        let result = morseToEnglish("h n d e")
+        expect(result).toBe("")
+    })
+
+    it("can translate and return full sentences", () => {
+        let result = morseToEnglish('- .... . / - .-. . . / .-- .- ... / - .- .-.. .-.. .-.-.- / - .... . / -.-. .- - / .-- .- ... / .-.. --- ..- -.. .-.-.-')
+        expect(result).toBe('The tree was tall. The cat was loud.')
+    })
 
 })
