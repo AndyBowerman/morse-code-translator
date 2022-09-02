febea9cc934ed75ccc0a57208058c37f49c78108
Andy Bowerman <andy_bowerman06@hotmail.com>
2022-09-02 12:45:34 +0100

    some amendments to the tests and completed the function

--- translator.js
+++ translator.js
@@ -1,5 +1,15 @@
-export const translateToMorse = () => {
+export const translateToMorse = (string) => {
+    const morseLookUp = {
+        a: '.-', b:'-...', c: '-.-.', d:'-..', e: '.', f: '..-.', g:'--.', h: '....', i: '..', j: '.---', k: '-.-', l: '.-..', m: '--',
+        n: '-.', o:'---', p: '.--.', q: '--.-', r: '.-.', s: '...', t: '-', u: '..-', v: '...-', w: '.--', x: '-..-', y: '-.--', z: '--..',
+        1: '.----', 2: '..---', 3: '...--', 4: '....-', 5: '.....', 6: '-....', 7: '--...', 8: '---..', 9: '----.', 0: '-----', '.': '.-.-.-',
+        ',': '--..--', '?': '..--..', "'": '.---.', '!': '-.-.--', "/": '-..-.', "(": '-.--.', ")": '-.--.-', "&": ".-...", ':': '---...',
+        ";": '-.-.-.', "=": '-...-', "+": '.-.-.', "-":'-....-', "_": '..--.-', '"': '.-..-.', "@": '.--.-.', " ": "/"
+    }
     
+    const arr = string.trim().split('');
+    const newArr = arr.map(item => morseLookUp[item.toLowerCase()] != undefined ? morseLookUp[item.toLowerCase()] + " " : "! ");
+    return newArr.includes('! ') ? `Warning, unknown symbol: ${newArr.join('').trim()}` : newArr.join('').trim();
 }
 
 module.exports = {translateToMorse};
--- translator.spec.js
+++ translator.spec.js
@@ -2,34 +2,39 @@ import { translateToMorse } from "./translator";
 
 
 describe("Testing translateToMorse", () => {
-    it("convert spaces to /", () => {
+    it("doesn't convert spaces if entered on their own", () => {
         let result = translateToMorse(" ")
-        expect(result).toBe("/")
+        expect(result).toBe("")
+    })
+
+    it("convert spaces to / when in a sentence", () => {
+        let result = translateToMorse("Good morning")
+        expect(result).toBe("--. --- --- -.. / -- --- .-. -. .. -. --.")
     })
 
     it("convert numbers to morse code", () => {
         let result = translateToMorse("1 2 3 4 5 6 7 8 9 0")
-        expect(result).toBe(".----/..---/...--/....-/...../-..../--.../---../----./-----")
+        expect(result).toBe(".---- / ..--- / ...-- / ....- / ..... / -.... / --... / ---.. / ----. / -----")
     })
 
     it("convert lowercase alphabet into morse code", () => {
         let result = translateToMorse('a b c d e f g h i j k l m n o p q r s t u v w x y z')
-        expect(result).toBe('.-/-.../-.-./-.././..-./--./..../../.---/-.-/.-../--/-./---/.--./--.-/.-./.../-/..-/...-/.--/-..-/-.--/--..')
+        expect(result).toBe('.- / -... / -.-. / -.. / . / ..-. / --. / .... / .. / .--- / -.- / .-.. / -- / -. / --- / .--. / --.- / .-. / ... / - / ..- / ...- / .-- / -..- / -.-- / --..')
     })
 
     it("convert uppercase alphabet into morse code", () => {
         let result = translateToMorse('A B C D E F G H I J K L M N O P Q R S T U V W X Y Z')
-        expect(result).toBe('.-/-.../-.-./-.././..-./--./..../../.---/-.-/.-../--/-./---/.--./--.-/.-./.../-/..-/...-/.--/-..-/-.--/--..')
+        expect(result).toBe('.- / -... / -.-. / -.. / . / ..-. / --. / .... / .. / .--- / -.- / .-.. / -- / -. / --- / .--. / --.- / .-. / ... / - / ..- / ...- / .-- / -..- / -.-- / --..')
     })
 
     it("convert punctuation into morse code", () => {
         let result = translateToMorse(". , ? ' ! / ( ) & : ; = + - _ \" @")
-        expect(result).toBe('.-.-.-/--..--/..--../.---./-.-.--/-..-./-.--./-.--.-/.-.../---.../-.-.-./-...-/.-.-./-....-/..--.-/.-..-./.--.-.')
+        expect(result).toBe('.-.-.- / --..-- / ..--.. / .---. / -.-.-- / -..-. / -.--. / -.--.- / .-... / ---... / -.-.-. / -...- / .-.-. / -....- / ..--.- / .-..-. / .--.-.')
     })
 
     it("return ! if the symbol is unrecognised", () => {
         let result = translateToMorse("£ $ % ^ ¬ `")
-        expect(result).toBe("!/!/!/!/!/!")
+        expect(result).toBe("Warning, unknown symbol: ! / ! / ! / ! / ! / !")
     })
 
     it("return letters within a word seperated by a space", () => {
